04446ff559c847eb4d814d6a38b587a06269e3e6
Michiel Van den Berghe <michiel@trackuity.com>
2019-02-21 21:31:59 +0100

    Make it prettier

--- garbage/config/defaults.py
+++ garbage/config/defaults.py
@@ -1,1 +1,4 @@
 CALENDAR_URL = None
+FONT_FILE = "garbage/data/free-fonts/wine-tahoma.ttf"
+# Put a timestamp on the generated image for debugging purposes
+SHOW_TIMESTAMP = False
--- garbage/server.py
+++ garbage/server.py
@@ -2,26 +2,46 @@ import time
 import asyncio
 import io
 from datetime import datetime
+from typing import List
 
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse, Response
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import uvicorn
 
 from garbage.calendar import Pickup, get_next_pickup
+from garbage.config import FONT_FILE, SHOW_TIMESTAMP
+
 
 app = Starlette()
 
 
+MAIN_FONT = ImageFont.truetype(FONT_FILE, 20)
+SMALL_FONT = ImageFont.truetype(FONT_FILE, 14)
+
+
+def get_garbage_image_path(garbage_types: List[str]) -> str:
+    if garbage_types == ["Restafval"]:
+        return 'garbage/data/trash-w.png'
+    return 'garbage/data/recycle-w.png'
+
+
 def generate_image(pickup: Pickup) -> Image:
     image = Image.new(mode='L', size=(200, 200), color=255)
-    trash_image = Image.open('garbage/data/trash-w.png')
-    image.paste(trash_image, (50, 30))
+    trash_image = Image.open(get_garbage_image_path(pickup.garbage_types))
+    image.paste(trash_image, (50, 24))
     draw = ImageDraw.Draw(image)
-    locale_time = datetime.isoformat(datetime.now()).split('.')[0]
-    draw.text((10,10), locale_time, fill=0)
-    draw.text((10, 160), ', '.join(pickup.garbage_types), fill=0)
-    draw.text((10, 170), pickup.date.humanize(locale='nl'), fill=0)
+    draw.fontmode = "0"
+    garbage_text = ', '.join(pickup.garbage_types)
+    human_date = pickup.date.humanize(locale='nl')
+    if SHOW_TIMESTAMP:
+        locale_time = datetime.isoformat(datetime.now()).split('.')[0]
+        w, h = draw.textsize(locale_time, font=SMALL_FONT)
+        draw.text((100 - w / 2,10), locale_time, fill=0, font=SMALL_FONT)
+    w, h = draw.textsize(garbage_text, font=MAIN_FONT)
+    draw.text((100 - w / 2, 130), garbage_text, fill=0, font=MAIN_FONT)
+    w, h = draw.textsize(human_date, font=SMALL_FONT)
+    draw.text((100 - w / 2, 160), human_date, fill=0, font=SMALL_FONT)
     return image
 
 
@@ -35,7 +55,6 @@ async def image(request) -> Response:
     ext = request.path_params['ext']
     loop = asyncio.get_running_loop()
     pickup = await get_next_pickup()
-    print(pickup)
     image = await loop.run_in_executor(None, generate_image, pickup)
     fp = io.BytesIO()
     format = Image.registered_extensions()[f'.{ext}']
